4e0d51014e8709261a46bba12ea8b06992364601
sakthipriyan07 <sakthi.priyan@vakilsearch.com>
2023-09-05 12:38:14 +0530

    update

--- Leadcreation2/src/test/java/Keymethods/Base.java
+++ Leadcreation2/src/test/java/Keymethods/Base.java
@@ -256,6 +256,8 @@ driver.findElement(By.xpath("//label[contains(text(),'No, I don’t have a company
 	}
 	public void helpdesk(WebDriver driver, String Username, String Mobilenumber, ExtentReports extentreport,
 			String Helpdeskuserid, String helpdeskpassword) throws InterruptedException, AWTException {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
 		test = extentreport.createTest("HelpDesk lead Verification Result");
 		driver.get("https://helpdesk.vakilsearch.com/login");
 		Robot robot = new Robot();
@@ -281,6 +283,8 @@ driver.findElement(By.xpath("//label[contains(text(),'No, I don’t have a company
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		Thread.sleep(8000);
 		try {
+			wait.until(
+					ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='search']")));
 			WebElement element501011 = driver.findElement(By.xpath("//input[@id='search']"));
 			JavascriptExecutor executor501011 = (JavascriptExecutor) driver;
 			executor501011.executeScript("arguments[0].click();", element501011);
